e75332fd22db40af3e940a7d4a8ceccb4df360e0
Dmtrii <kopenkin.da@gmail.com>
2021-10-12 11:31:19 +0200

    # Activity 2 + Quit blocking

--- dev-mobile/cours-02/app/src/main/java/com/example/cours2/MainActivity.java
+++ dev-mobile/cours-02/app/src/main/java/com/example/cours2/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.cours2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "SUS";
     private Button goButton;
@@ -16,17 +19,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goButton = (Button)findViewById(R.id.GO_BUTTON);
-        // 1
-        goButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG, "View.OnClickListener");
-            }
-        });
-        // 1.5
+        goButton = (Button) findViewById(R.id.GO_BUTTON);
         goButton.setOnClickListener(v -> {
-            Log.v(TAG, "v -> ");
+            Intent intent = new Intent(this, Act2.class);
+            startActivity(intent);
         });
 
     }
@@ -34,17 +30,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // 2
-        goButton.setOnClickListener(null);
-    }
-
-    public void onClickXML(View v) {
-        Log.v(TAG, "onClickXML");
     }
 
     @Override
-    public void onClick(View v) {
-        // 3
-        Log.v(TAG, "onClick override");
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("BRUH")
+                .setMessage("You leavin' huh?")
+                .setPositiveButton("What about it?", (dialog, which) -> finish())
+                .setNegativeButton("I'm sorry", null)
+                .show();
     }
 }
