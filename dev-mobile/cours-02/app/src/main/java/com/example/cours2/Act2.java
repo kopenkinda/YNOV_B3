2a02be87aacb42200d672a8bff2c10695c2df6c3
Dmtrii <kopenkin.da@gmail.com>
2021-10-12 12:21:10 +0200

    WEbViewactivity

--- dev-mobile/cours-02/app/src/main/java/com/example/cours2/Act2.java
+++ dev-mobile/cours-02/app/src/main/java/com/example/cours2/Act2.java
@@ -3,6 +3,7 @@ package com.example.cours2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Act2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Act2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act2);
+        WebView webview = (WebView) findViewById(R.id.WEB_VIEW);
+        webview.getSettings().setJavaScriptEnabled(false);
+        webview.loadUrl("https://www.google.com");
     }
 }
