01cffc4b1803ba75cef07d5356c0e3a605133f68
kopenkinda <kopenkin.da@gmail.com>
2022-03-15 16:32:49 +0100

    added middleware

--- nodejs/server-express.mjs
+++ nodejs/server-express.mjs
@@ -1,14 +1,40 @@
 import express from "express";
+import luxon from "luxon";
+import { birthdayDaysWithPhase } from "./fns-lunar.mjs";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+class HttpError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+  }
+}
+
+const verifyParams = (req, res, next) => {
+  const { city, dateFrom } = req.query;
+  if (city === undefined || dateFrom === undefined) {
+    return next(new HttpError("Bad request", 400));
+  }
+  if (city === "" || dateFrom === "") {
+    return next(new HttpError("Fields cannot be empty", 400));
+  }
+  if (!luxon.DateTime.fromISO(dateFrom).isValid) {
+    return next(new HttpError("Invalid date format", 400));
+  }
+  next();
+};
+
+const errorHandler = (err, req, res, next) => {
+  res.status(err.status ?? 500).json({ error: err.message });
+};
+
+app.get("/bdays", verifyParams, async (req, res) => {
+  const { city, dateFrom } = req.query;
+  return res.json(await birthdayDaysWithPhase(dateFrom, city));
 });
 
-app.get("/theo", (req, res) => {
-  res.send("♿️");
-});
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on: http://localhost:3000");
